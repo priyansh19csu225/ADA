b1635b54554627591205607ddc8cd6661babb32c
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2021-05-09 15:34:20 +0530

    Heap sort

--- ADA lab1/src/lab6.java
+++ ADA lab1/src/lab6.java
@@ -25,7 +25,7 @@ public class lab6 {
  
  
         for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+            heapify( arr, n, i);
  
     
         for (int i = n - 1; i > 0; i--) {
